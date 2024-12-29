a4a7eab7aa232867cc7c08d318c2872644a78167
Anwesh43 <anweshthecool0@gmail.com>
2024-12-29 18:21:42 +0530

    adding logic for saving image

--- app.ts
+++ app.ts
@@ -11,14 +11,14 @@ const app: Express = express()
 
 const imageService = getInstance()
 
-app.use(bodyParser.json())
+app.use(bodyParser.json({ limit: '50mb' }))
 
 app.use(express.static(path.join(__dirname, 'public')))
 
 app.post("/saveImage", (req: express.Request, res: express.Response) => {
     const imageData: string = req.body['imageData']
     try {
-        const fileName = imageService.saveImage(imageData)
+        const fileName = imageService.saveImage(imageData.split(",")[1])
         res.status(200).json({
             status: 'success',
             fileName,
@@ -36,6 +36,7 @@ app.post('/editImage', async (req: express.Request, res: express.Response) => {
     const maskPath = req.body['maskPath']
     const prompt = req.body["prompt"]
     try {
+        console.log("STARTING_TO_GENERATE_IMAGE")
         const response = await imageService.generateEdittedImage(imagePath, maskPath, prompt)
         console.log("RESPONSE", response.data)
         res.status(200).json({
@@ -43,6 +44,9 @@ app.post('/editImage', async (req: express.Request, res: express.Response) => {
         })
     } catch (error) {
         console.error(error)
+        res.status(500).json({
+            status: "error"
+        })
 
     }
 })
--- imageService.ts
+++ imageService.ts
@@ -1,7 +1,11 @@
-import { createReadStream, writeFileSync } from "fs"
+import { createReadStream, Stats, statSync, writeFileSync } from "fs"
 import { OpenAI } from 'openai'
 import { Images } from "openai/resources"
 
+const getFileSize = (imagePath: string) => {
+    const fileData: Stats = statSync(imagePath)
+    console.log(imagePath, fileData.size / (1024 * 1024))
+}
 class ImageService {
     openai: OpenAI
     constructor() {
@@ -15,16 +19,20 @@ class ImageService {
     }
 
     async generateEdittedImage(imagePath: string, maskPath: string, prompt: string) {
-        const imageResponse: OpenAI.Images.ImagesResponse = await this.openai.images.edit({
+        getFileSize(imagePath)
+        getFileSize(maskPath)
+        const params: OpenAI.Images.ImageEditParams = {
             model: 'dall-e-2',
             image: createReadStream(imagePath),
             mask: createReadStream(maskPath),
             n: 1,
             size: '512x512',
             prompt,
-        })
+        };
+        const imageResponse: OpenAI.Images.ImagesResponse = await this.openai.images.edit(params)
+        console.log("IMAGE_RESPONSE", imageResponse)
         return {
-            data: imageResponse.data[0].b64_json,
+            data: imageResponse.data[0].url,
             created: imageResponse.created
         }
     }
